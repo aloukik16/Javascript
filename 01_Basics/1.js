3b42b76178a6274bdf7cf5c562d9201861da13f2
Aloukik Agrawal <aloukikagrawal90@gmail.com>
2024-04-17 14:39:27 +0530

    01

--- 01_Basics/1.js
+++ 01_Basics/1.js
@@ -1,1 +1,7 @@
-console.log("Hello, World");
+const accountId = 144553 // Cannot change.
+let accountEmail = "test@gmail.com" // We can change it.
+var  aaccountPassword = "12345" // Currently not in use Because of issue  in block scope and fuctional scope.
+accounCity = "Jaipur"
+let accountState;
+
+console.table([accountId, accountEmail, aaccountPassword, accounCity, accountState]);
