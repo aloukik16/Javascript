b55da479ea8ecc454e99584e762f64eb09020b9a
Aloukik Agrawal <aloukikagrawal90@gmail.com>
2024-04-17 14:49:00 +0530

    02

---
+++ 01_Basics/2.js
@@ -0,0 +1,17 @@
+"use strict"; // Treat all JS code as newer version.
+
+// alert (3+3) we are using nodejs, not browser.
+// Ecmascript (tc39) writes the standard of JS.
+
+// Datatypes:
+// number - 2 to power 53
+// bigint
+// string - ""
+// boolean - true/false
+// null - standalone Value
+// undefined
+// symbol - unique
+
+// Object:
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
