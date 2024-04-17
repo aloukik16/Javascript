6cadca78f15f699db81c1513f59df677f8675e83
Aloukik Agrawal <aloukikagrawal90@gmail.com>
2024-04-17 15:05:19 +0530

    04

---
+++ 01_Basics/4.js
@@ -0,0 +1,10 @@
+let value = 3
+let negative_value = -value
+console.log(negative_value);
+
+let str1 = "Hello"
+let str2 = " Aloukik"
+console.log(str1 + str2);
+
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2"); // The First Place will be Taken.
