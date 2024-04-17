5bb1bf55b80171980505a9c75c5fa409177e714d
Aloukik Agrawal <aloukikagrawal90@gmail.com>
2024-04-17 15:36:30 +0530

    06

---
+++ 01_Basics/6.js
@@ -0,0 +1,13 @@
+const name = "Aloukik"
+const age = 18
+
+console.log(`Hello my name is ${name} and my age is ${age}.`);
+// toUpper
+// charAt
+// indexOf
+// substring
+// slice
+// trim()
+// replace
+// includes
+// split
