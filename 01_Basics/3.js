da548c4695e6984b2e8ad410c7482f8c00c8f051
Aloukik Agrawal <aloukikagrawal90@gmail.com>
2024-04-17 14:56:59 +0530

    03

---
+++ 01_Basics/3.js
@@ -0,0 +1,11 @@
+let score = "33abc"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+
+console.log(valueInNumber) // NaN Not a number.
+// For null - 0
+// For undefined - Nan
+// For boolean = true - 1/ false - 0
+// For empty "" - false
