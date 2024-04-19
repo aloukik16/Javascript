95bf3830a1c2e31bad1a6c075087de82e1169c00
Aloukik Agrawal <aloukikagrawal90@gmail.com>
2024-04-19 12:58:46 +0530

    01_arrays basics

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,46 @@
+// array 
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["Shaktiman", "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+
+// console.log(myArr);
+
+// Array methods 
+
+// myArr.push(6)
+// console.log(myArr);
+// myArr.push(7)
+// console.log(myArr);
+// myArr.pop()
+// console.log(myArr);
+
+// console.log(myArr.unshift(9))
+// console.log(myArr.shift())
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+// console.log(newArr)
+
+// slice, splice
+
+console.log("A", myArr);
+
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+
+console.log(myn1);
+console.log("B ", myArr);
+console.log("====================")
+
+const myn2 = myArr.splice(0, 2)
+console.log("C", myArr);
+console.log(myn2);
+
+
+
+
