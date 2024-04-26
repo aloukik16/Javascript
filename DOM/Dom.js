0fe379277ef908276dab0104cc8b8f095c838f80
Aloukik Agrawal <aloukikagrawal90@gmail.com>
2024-04-26 14:31:58 +0530

    DOM

---
+++ DOM/Dom.js
@@ -0,0 +1,21 @@
+// EXAMINE THE DOCUMENT ONJECT //
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// document.all[10].textContent = "Hello, World";
+// console.log(document.forms);
+// console.log(document.links);
+
+
+// GETELEMENTBYID //
+// var headerTitle = document.getElementById('header-title');
+// console.log(headerTitle);
+// headerTitle.textContent = 'Hello';
+// headerTitle.innerText = 'GoodBye';
